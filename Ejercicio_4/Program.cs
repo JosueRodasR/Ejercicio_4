05d6fdc47564beaa375b1724bc1551acc85c8194
Usuario <Usuario@DESKTOP-U4P8J7O>
2023-08-28 22:09:06 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio_4/Program.cs
@@ -0,0 +1,29 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int alumnosPorSalon = 40; 
+            int tiempoSalonA = 5;
+            int tiempoSalonB = 8;
+            int tiempoSalonC = 6;
+
+            int totalAlumnos = 3 * alumnosPorSalon;
+
+            int tiempoTotalMinutos = (alumnosPorSalon *tiempoSalonA) + (alumnosPorSalon* tiempoSalonB) + (alumnosPorSalon * tiempoSalonC);
+            
+            int horas = tiempoTotalMinutos / 60;
+            int minutos = tiempoTotalMinutos % 60;
+
+            Console.WriteLine($"El instructor se tardara en revisar {horas} horas y {minutos} minutos en revisar todos los examenes.");
+            Console.ReadKey();
+        }
+    }
+}
